eb2b23af510c797461f52dcb15e2736e2a17b83f
trebektas <trensarbektas@gmail.com>
2023-05-16 16:15:54 +0200

    re-create react app with server

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import * as http from "http";
+import cors from "cors";
+import connectDB from "./db/connectDB.js";
+import dotenv from "dotenv";
+import { Server } from "socket.io";
+
+dotenv.config();
+
+// The environment should set the port
+const port = process.env.PORT;
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+if (port == null) {
+  // If this fails, make sure you have created a `.env` file in the right place with the PORT set
+  console.log(
+    new Error("Cannot find a PORT number, did you create a .env file?")
+  );
+}
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    server.listen(3001, () => {
+      console.log(`Server started on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
